877194a4ab10c4738bf946fe96f760bf58cf0139
Mae Fang <+>
2021-06-04 01:05:09 -0400

    create an express server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const getTents = require('./services/notion')
+
+//set up a simple express server, define a port OR port 3000
+const express = require('express')
+const PORT = process.env.PORT || 3000
+
+//initialize server
+const app = express()
+
+app.listen(PORT, console.log(`server started on port ${PORT}`)
