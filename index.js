350f3fcdfb8d420d3403859078bff30910c8a91c
Mae Fang <+>
2021-06-03 23:45:02 -0400

    retrieve specified tent options db

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const dotenv = require('dotenv').config()
+const {Client} = require('@notionhq/client')
+
+const notion = new Client({
+    auth: process.env.NOTION_TOKEN
+})
+
+//From Notion's example
+const listUsers = async () => {
+    const listUsersResponse = await notion.users.list()
+};
+
+//Grab all the lists of my tent options plan
+
+const retrieveCampingList =  async () => {
+    const allCampLists = await notion.databases.query({
+        database_id: process.env.NOTION_DATABASE_ID,  
+    }) 
+    console.log(allCampLists)
+};
+ retrieveCampingList()
