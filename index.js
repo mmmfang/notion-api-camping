70e764d4726b8e008994e44445fcfcf8a70006e8
Mae Fang <+>
2021-06-05 19:18:37 -0400

    create endpoint of /tents with express

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const getTents = require('./services/notion')
-
+const getTents = require('./services/notion.js')
 //set up a simple express server, define a port OR port 3000
 const express = require('express')
 const PORT = process.env.PORT || 3000
 
+
+
 //initialize server
 const app = express()
 
-app.listen(PORT, console.log(`server started on port ${PORT}`)
+app.get('/tents', async (req, res) => {
+    const tents = await getTents();
+    res.json(tents);
+})
+//now we can go to localhost:3000/tents and see the tent object we want
+
+app.listen(PORT, console.log(`server started on port ${PORT}`))
--- services/notion.js
+++ services/notion.js
@@ -38,14 +38,14 @@ module.exports = async function getTentInfo() {
     //    const tentName = result.properties['Tent Name'].title[0].plain_text;
     //    console.log(`${tentName} is ${tentWeight} and costs ${tentPrice} at ${tentRetailer}`)
     //})
-
     //Option 2 - using map (based on Traversy media tutorial)
     const tentCard  = results.map(result => {
         return {
-            tentPrice: result.properties.Price.number,
-            tentWeight: result.properties['Weight (lbs)'].number,
-            tentRetailer: result.properties.Retailer.rich_text[0].plain_text,
             tentName: result.properties['Tent Name'].title[0].plain_text,
+            tentWeight: result.properties['Weight (lbs)'].number,
+            tentPrice: result.properties.Price.number,            
+            tentRetailer: result.properties.Retailer.rich_text[0].plain_text,
         }
     })
+    return tentCard
 }
